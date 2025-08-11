850f5cbe0e905d213e908c78255fd41e990a81c6
alissonFabricio04 <alissonfabricio.dev@gmail.com>
2025-08-11 20:31:10 -0300

    Implement CreateProduct feature

---
+++ apps/api/modules/products/features/create.go
@@ -0,0 +1,44 @@
+package features
+
+import (
+	"example/apps/api/modules/auth/locals"
+	"example/libs/database"
+	"example/libs/database/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type CreateProductBodyDTO struct {
+	Name     string `validate:"required" json:"name"`
+	Price    int    `validate:"required" json:"price"`
+	Quantity int    `validate:"required" json:"quantity"`
+}
+
+func CreateProduct(c *fiber.Ctx) error {
+	body := c.Locals("body").(*CreateProductBodyDTO)
+	userId := c.Locals(locals.UserIdLocal).(int)
+
+	if body.Price < 0 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid product price",
+		})
+	}
+
+	product := models.Product{
+		Name:     body.Name,
+		Price:    body.Price,
+		Quantity: body.Quantity,
+		UserID:   userId,
+	}
+
+	if err := database.DB.Create(&product).Error; err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to create product",
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Product created successfully",
+		"product": product,
+	})
+}
