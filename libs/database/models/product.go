76fd883660bf987a016d93ebe28d282e3d0cb1d6
alissonFabricio04 <alissonfabricio.dev@gmail.com>
2025-08-12 18:11:26 -0300

    Fix JSON tags for Product model fields to ensure consistency in naming conventions

--- libs/database/models/product.go
+++ libs/database/models/product.go
@@ -1,12 +1,23 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
 
+	"gorm.io/gorm"
+)
+
+/**
+ * @todo: esqueci de add a tag json aos campos do gorm, por exemplo: CreatedAt.
+ * Entao esses campos estao como camelCase enquanto o restante esta em snake_case.
+ */
 type Product struct {
 	gorm.Model
-	ID       int     `json:"id"`
-	Name     string  `json:"name"`
-	Price    float64 `gorm:"type:double" json:"price"`
-	Quantity int     `json:"quantity"`
-	UserID   int     `json:"user_id"`
+	ID        int        `json:"id"`
+	Name      string     `json:"name"`
+	Price     float64    `gorm:"type:double" json:"price"`
+	Quantity  int        `json:"quantity"`
+	UserID    int        `json:"user_id"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `gorm:"index" json:"deleted_at"`
 }
