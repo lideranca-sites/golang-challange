8fd0aa53d4134f6c7e118e55d17e8117d0a1f2d5
alissonFabricio04 <alissonfabricio.dev@gmail.com>
2025-08-12 16:40:40 -0300

    Add validation for update product request and update routes

--- apps/api/modules/products/routes.go
+++ apps/api/modules/products/routes.go
@@ -4,6 +4,7 @@ import (
 	"example/apps/api/infra/validation"
 	"example/apps/api/modules/auth/middleware"
 	"example/apps/api/modules/products/features"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -12,9 +13,15 @@ func validateCreateProductRequest(c *fiber.Ctx) error {
 	return validation.ValidateBody(c, &features.CreateProductBodyDTO{})
 }
 
+func validateUpdateProductRequest(c *fiber.Ctx) error {
+	return validation.ValidateBody(c, &features.UpdateProductBodyDTO{})
+}
+
 const BASE_PATH = "/products"
 
 func SetupRoutes(router fiber.Router) {
 	router.Get(BASE_PATH, features.ListProducts)
 	router.Post(BASE_PATH, middleware.JWTProtected, validateCreateProductRequest, features.CreateProduct)
+	productIdPath := fmt.Sprintf("%s/:id", BASE_PATH)
+	router.Put(productIdPath, middleware.JWTProtected, validateUpdateProductRequest, features.UpdateProduct)
 }
