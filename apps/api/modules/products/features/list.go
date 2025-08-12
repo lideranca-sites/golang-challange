a737879d59587dfff8c0d0ca253eb0011d9048fd
alissonFabricio04 <alissonfabricio.dev@gmail.com>
2025-08-12 18:24:46 -0300

    Refactor ListProducts to return structured product response without deleted_at field

--- apps/api/modules/products/features/list.go
+++ apps/api/modules/products/features/list.go
@@ -4,6 +4,7 @@ import (
 	"example/libs/database"
 	"example/libs/database/models"
 	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -29,8 +30,31 @@ func ListProducts(c *fiber.Ctx) error {
 		})
 	}
 
+	// remove deleted_at field
+	type ProductResponse struct {
+		ID        int       `json:"id"`
+		Name      string    `json:"name"`
+		Price     float64   `json:"price"`
+		Quantity  int       `json:"quantity"`
+		UserID    int       `json:"user_id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	}
+	productsResponse := make([]ProductResponse, 0, len(products))
+	for i := range products {
+		productsResponse = append(productsResponse, ProductResponse{
+			ID:        products[i].ID,
+			Name:      products[i].Name,
+			Price:     products[i].Price,
+			Quantity:  products[i].Quantity,
+			UserID:    products[i].UserID,
+			CreatedAt: products[i].CreatedAt,
+			UpdatedAt: products[i].UpdatedAt,
+		})
+	}
+
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"products": products,
+		"products": productsResponse,
 	})
 }
 
