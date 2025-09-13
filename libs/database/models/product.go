15471c6597b9c566b091d0f1d4a0c66096164eac
EduSG <eduardo.gomes.072020@gmail.com>
2025-09-13 17:30:28 -0300

    feat: implementação de novos campos na model

--- libs/database/models/product.go
+++ libs/database/models/product.go
@@ -1,10 +1,9 @@
 package models
 
-import "gorm.io/gorm"
-
 type Product struct {
-	gorm.Model
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	UserID int    `json:"user_id"`
+	ID       int     `json:"id" gorm:"primaryKey"`
+	Name     string  `json:"name"`
+	Price    float64 `json:"price"`
+	Quantity int     `json:"quantity"`
+	UserID   int     `json:"user_id"`
 }
