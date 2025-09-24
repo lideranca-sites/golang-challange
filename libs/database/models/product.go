96d3f97ebe8d22eb74e720a43210452b9e5567c6
MaykPlatero <maykp.platero1910@gmail.com>
2025-09-23 23:28:29 -0300

    feat: model adjustment

--- libs/database/models/product.go
+++ libs/database/models/product.go
@@ -1,10 +1,13 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Product struct {
 	gorm.Model
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	UserID int    `json:"user_id"`
+	Name     string  `json:"name"`
+	Price    float64 `json:"price"`
+	Quantity int     `json:"quantity"`
+	UserID   int     `json:"user_id"`
 }
