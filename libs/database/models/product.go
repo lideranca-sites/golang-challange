eff39348e349faf0b1130b7e23d8ef149e6e6ac0
MaykPlatero <maykp.platero1910@gmail.com>
2025-09-25 08:12:54 -0300

    refactor: add gorm.Model

--- libs/database/models/product.go
+++ libs/database/models/product.go
@@ -1,14 +1,13 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+)
 
 type Product struct {
-	ID        uint       `json:"id" gorm:"primarykey"`
-	Name      string     `json:"name"`
-	Price     float64    `json:"price"`
-	Quantity  int        `json:"quantity"`
-	UserID    int        `json:"user_id"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at,omitempty" gorm:"index"`
+	gorm.Model
+	Name     string  `json:"name"`
+	Price    float64 `json:"price"`
+	Quantity int     `json:"quantity"`
+	UserID   int     `json:"user_id"`
 }
