a7aeb32a4c8f5e80428e8010875307e8361ad44f
Elton Moreno <emoreno.heimdall@lidercap.com.br>
2025-12-12 14:21:06 -0300

    refactor: refatoração das models existentes

--- libs/database/models/product.go
+++ libs/database/models/product.go
@@ -1,10 +1,37 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"example/apps/api/domain/dto"
+	"example/apps/api/domain/entities"
+
+	"gorm.io/gorm"
+)
 
 type Product struct {
 	gorm.Model
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	UserID int    `json:"user_id"`
+	ID       uint `gorm:"primaryKey"`
+	Name     string
+	UserID   uint
+	Price    float64 `gorm:"type:decimal(10,2)"`
+	Quantity int
+}
+
+func (p Product) ToEntity() entities.ProductEntity {
+	return entities.ProductEntity{
+		ID:       p.ID,
+		Name:     p.Name,
+		Price:    p.Price,
+		Quantity: p.Quantity,
+		UserID:   p.UserID,
+	}
+}
+
+func ProductModelFromDto(input dto.ProductDTO) Product {
+	return Product{
+		ID:       input.ID,
+		Name:     input.Name,
+		Price:    input.Price,
+		Quantity: input.Quantity,
+		UserID:   input.UserID,
+	}
 }
