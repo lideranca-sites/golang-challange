480cb6d446bcf700bafc31e0c5554db5716d1088
Guilherme Torres <gntorres.raro@lidercap.com.br>
2025-12-15 10:24:54 -0300

    feat: added basic crud operations for the product entity

---
+++ apps/api/modules/products/delete-product.go
@@ -0,0 +1,25 @@
+package products
+
+import (
+    "strconv"
+    "example/libs/database"
+    "example/libs/database/models"
+    "github.com/gofiber/fiber/v2"
+)
+
+func DeleteProduct(c *fiber.Ctx) error {
+    idStr := c.Params("id")
+    id, err := strconv.Atoi(idStr)
+
+    if err != nil {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid id"})
+    }
+
+    result := database.DB.Delete(&models.Product{}, id)
+
+    if result.Error != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to delete product"})
+    }
+
+    return c.SendStatus(fiber.StatusNoContent)
+}
---
+++ apps/api/modules/products/get-products.go
@@ -0,0 +1,29 @@
+package products
+
+import (
+    "example/libs/database"
+    "example/libs/database/models"
+    "github.com/gofiber/fiber/v2"
+)
+
+func GetProducts(c *fiber.Ctx) error {
+    var query struct {
+        UserId *int `query:"user_id"`
+    }
+
+    if err := c.QueryParser(&query); err != nil {
+        return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+    }
+
+    var products []models.Product
+
+    if query.UserId != nil {
+        database.DB.Where("user_id = ?", *query.UserId).Find(&products)
+    } else {
+        database.DB.Find(&products)
+    }
+
+    return c.Status(fiber.StatusOK).JSON(fiber.Map{
+        "products": products,
+    })
+}
